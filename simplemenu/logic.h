c3c41a587ab3cb1a3ba8cb05d792722bd78f8b6b
bittboy <bittboy@bittboy-pc>
2019-08-10 15:22:50 -0300

    Remove obsolete headers

--- simplemenu/logic.h
+++ simplemenu/logic.h
@@ -1,8 +1,6 @@
 #ifndef LOGIC
 #define LOGIC
-void readConfig();
 void quit();
-void executeFavorite (char fileToBeExecutedWithFullPath[]);
 void executeCommand (char executable[], char fileToBeExecutedWithFullPath[]);
 void loadGameList();
 #endif
