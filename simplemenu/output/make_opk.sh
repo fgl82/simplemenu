35498672d6134359b3ca11705599e17ebe78dc2a
bittboy <bittboy@bittboy-pc>
2019-11-16 16:20:16 -0300

    OPK and whatever

---
+++ simplemenu/output/make_opk.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+OPK_NAME='simplemenu.opk'
+
+echo ${OPK_NAME}
+
+# create default.gcw0.desktop
+cat > default.gcw0.desktop <<EOF
+[Desktop Entry]
+Name=SimpleMenu
+Comment=A sensible launcher
+Exec=simplemenu.dge
+Terminal=false
+Type=Application
+StartupNotify=true
+Icon=smenu
+Categories=applications;
+EOF
+
+# create opk
+FLIST="simplemenu.dge"
+FLIST="${FLIST} invoker.dge"
+FLIST="${FLIST} usb_mode_on.sh"
+FLIST="${FLIST} usb_mode_off.sh"
+FLIST="${FLIST} resources"
+FLIST="${FLIST} config"
+FLIST="${FLIST} default.gcw0.desktop"
+FLIST="${FLIST} smenu.png"
+
+rm -f ${OPK_NAME}
+mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
+
+cat default.gcw0.desktop
+rm -f default.gcw0.desktop
