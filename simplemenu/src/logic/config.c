17f01c816a0858df344191b3af1711eab8d9eef6
bittboy <bittboy@bittboy.com>
2021-08-25 00:11:01 -0300

    Last touches

--- simplemenu/src/logic/config.c
+++ simplemenu/src/logic/config.c
@@ -1376,7 +1376,7 @@ void saveLastState() {
 	char pathToStatesFilePlusFileName[300];
 	snprintf(pathToStatesFilePlusFileName,sizeof(pathToStatesFilePlusFileName),"%s/.simplemenu/last_state.sav",home);
 	fp = fopen(pathToStatesFilePlusFileName, "w");
-	fprintf(fp, "%d;\n", 61);
+	fprintf(fp, "%d;\n", 10);
 	fprintf(fp, "%d;\n", stripGames);
 	fprintf(fp, "%d;\n", fullscreenMode);
 	fprintf(fp, "%d;\n", footerVisibleInFullscreenMode);
@@ -1439,7 +1439,7 @@ void loadLastState() {
 		}
 		if (savedVersion==-1) {
 			savedVersion=atoifgl(configurations[0]);
-			if(savedVersion!=61) {
+			if(savedVersion!=10) {
 				saveLastState();
 				fclose(fp);
 				if (line) {
--- simplemenu/src/logic/system_logic_bittboy.c
+++ simplemenu/src/logic/system_logic_bittboy.c
@@ -388,20 +388,20 @@ int getBatteryLevel() {
 	int max_voltage = 4050;
 	int min_voltage = 3480;
 	int voltage_now;
-	int charging=0;
 	int total;
-	FILE *f = fopen("/sys/class/power_supply/miyoo-battery/voltage_max_design", "r");
-
-	f = fopen("/sys/class/power_supply/miyoo-battery/voltage_now", "r");
+	FILE *f = fopen("/sys/class/power_supply/miyoo-battery/voltage_now", "r");
 	fscanf(f, "%i", &voltage_now);
 	fclose(f);
 
-	total = (voltage_now - min_voltage) * 6 / (max_voltage - min_voltage);
-	if (charging==1) {
-		return 6;
-	}
+//	total = (voltage_now - min_voltage) * 6 / (max_voltage - min_voltage);
+	if (voltage_now > 4050) return 6;
+	if (voltage_now > 4000) return 5;
+	if (voltage_now > 3900) return 4;
+	if (voltage_now > 3800) return 3;
+	if (voltage_now > 3700) return 2;
+	if (voltage_now > 3520) return 1;
 //	if (total>5) {
 //		return 5;
 //	}
-	return total;
+//	return total;
 }
