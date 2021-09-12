5b4fa1d081f1581cc59f8909511b5d675fb66a66
bittboy <bittboy@bittboy.com>
2021-09-12 20:24:05 -0300

    Better script

--- simplemenu/output/make_bittboy.sh
+++ simplemenu/output/make_bittboy.sh
@@ -2,9 +2,15 @@
 cd ..
 make clean
 make PLATFORM=BITTBOY
-cp output/simplemenu /media/bittboy/main/apps/simplemenu
+cp output/simplemenu /media/bittboy/main/apps/simplemenu/
+cd ..
+cd ..
+cd invoker
+cd invoker
+make clean
+make PLATFORM=BITTBOY
+cp invoker.dge /media/bittboy/main/apps/simplemenu/
 umount /media/bittboy/boot
 umount /media/bittboy/main
 umount /media/bittboy/rootfs
-cd output
 
